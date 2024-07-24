a4fce8d352a6bd0c29f67470ebf331a8da98e169
troh <dhxofla91@gmail.com>
2024-07-24 16:58:34 +0900

    그래프 - 이코테

---
+++ src/c_graph/DFS예제_이코테.java
@@ -0,0 +1,34 @@
+package c_graph;
+
+public class DFS예제_이코테 {
+    public static void main(String[] args) {
+        int[][] graph = {
+                {},
+                {2, 3, 8},
+                {1, 7},
+                {1, 4, 5},
+                {3, 5},
+                {3, 4},
+                {7},
+                {2, 6, 8},
+                {1, 7}
+        };
+
+        boolean[] visited = new boolean[10];
+
+        DFS(graph, 1, visited);
+    }
+
+    private static void DFS(int[][] graph, int v, boolean[] visited) {
+        if(visited[v]) return;
+
+        visited[v] = true;
+        System.out.println("v = " + v);
+
+        for (int nv : graph[v]) {
+            if(!visited[nv]) {
+                DFS(graph, nv, visited);
+            }
+        }
+    }
+}
